acde20c9d07e6785f2e3763a993b39ba01072b35
Vladimir Samojlov <v-moi@ya.ru>
2018-12-16 17:41:30 +0200

    contact edit

--- blocks/contact.php
+++ blocks/contact.php
@@ -9,17 +9,17 @@
 	<div class="contact" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
 		<div class="row justify-content-center mb-5">
 			<div class="col-md-7 d-flex justify-content-center flex-column">
-				<p>Для организаторов, авторов и творческого сотрудничества:</p>
 				Директор Ольга Коняхина
 				<br>+7 (917) 598-13-13
 				<br>director@golitsyna.ru
 				<br>9175981313@mail.ru
 				<br><br>Администрация:
-				<br>+79154990191 только сообщения, вотсап, вайбер.
+				<br>+79154990191 только сообщения, whatsapp, viber.
 			</div>
 		</div>
 		<div class="row justify-content-center">
-			<div class="col-md-7 d-flex justify-content-center">
+			<div class="col-md-7 d-flex justify-content-center flex-column">
+				<p>Для организаторов, авторов и творческого сотрудничества:</p>	
 				<?php echo do_shortcode('[contact-form-7 id="73" title="Контакты"]'); ?>
 			</div>
 		</div>	
